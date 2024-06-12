7c7b63fad24b25202b8703631690059d3cea1c23
Pratheek <159915259+pratheekpoojary23@users.noreply.github.com>
2024-06-12 14:24:27 +0530

    Add files via upload

---
+++ pattern_using_recursion.py
@@ -0,0 +1,11 @@
+def print_pattern(n,m):
+    if n==0:
+        return
+    for i in range(5,m-1,-1):
+        print(i,end=" ")
+    print()
+    print_pattern(n-1,m+1)
+    
+n=5
+m=1
+print_pattern(n,m)
